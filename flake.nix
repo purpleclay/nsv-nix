df26bed7760eefa0c0126f4149516e4853f4fbb3
purpleclay <purpleclaygh@gmail.com>
2024-11-23 07:47:41 +0000

    onboard nsv nur

--- flake.nix
+++ flake.nix
@@ -1,15 +1,15 @@
 {
-  description = "My personal NUR repository";
+  description = "NSV NUR Repository";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   outputs = { self, nixpkgs }:
     let
       systems = [
         "x86_64-linux"
-        "i686-linux"
-        "x86_64-darwin"
         "aarch64-linux"
-        "armv6l-linux"
         "armv7l-linux"
+        "x86_64-darwin"
+        "armv7l-darwin"
+        "aarch64-darwin"
       ];
       forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
     in
